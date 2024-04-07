96f952587de56e80770e19fd3eb5e0ba8aaff31c
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-04-07 20:09:12 -0300

    se cambio el funcionamiento de SongDescriptionHelperImpl

--- app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
+++ app/src/main/java/ayds/songinfo/home/view/SongDescriptionHelperImpl.kt
@@ -3,6 +3,7 @@ package ayds.songinfo.home.view
 import ayds.songinfo.home.model.entities.Song.EmptySong
 import ayds.songinfo.home.model.entities.Song
 import ayds.songinfo.home.model.entities.Song.SpotifySong
+import java.text.DateFormatSymbols
 
 interface SongDescriptionHelper {
     fun getSongDescriptionText(song: Song = EmptySong): String
@@ -11,16 +12,33 @@ interface SongDescriptionHelper {
 internal class SongDescriptionHelperImpl : SongDescriptionHelper {
     override fun getSongDescriptionText(song: Song): String {
         return when (song) {
-            is SpotifySong ->
-                "${
-                    "Song: ${song.songName} " +
-                            if (song.isLocallyStored) "[*]" else ""
-                }\n" +
+            is SpotifySong -> {
+                val releaseDateParts = song.releaseDate.split("-")
+                val year = releaseDateParts.first()
+                val month = releaseDateParts.getOrNull(1)?.toIntOrNull()?.let { monthNumber ->
+                    DateFormatSymbols().months[monthNumber - 1]
+                }
+
+                val releaseDateText = when (song.releaseDatePresicion) {
+                    "year" -> {
+                        "$year (not a leap year)"
+                    }
+                    "month" -> {
+                        "$month, $year"
+                    }
+                    "day" -> {
+                        "${releaseDateParts[2]}/${releaseDateParts[1]}/$year"
+                    }
+                    else -> song.releaseDate
+                }
+
+                "Song: ${song.songName} ${if (song.isLocallyStored) "[*]" else ""}\n" +
                         "Artist: ${song.artistName}\n" +
                         "Album: ${song.albumName}\n" +
-                        "Year: ${song.year}"
+                        "Release date: $releaseDateText"
+            }
             else -> "Song not found"
         }
-
     }
+
 }

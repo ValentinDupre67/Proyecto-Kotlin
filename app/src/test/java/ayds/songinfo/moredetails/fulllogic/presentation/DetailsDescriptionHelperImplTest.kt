88357e85445f72244e940853541c631967303a2a
Franco Feuilles <francofeuilles@gmail.com>
2024-05-17 09:13:50 -0300

    Se agregaron los test para la clase DetailsDescriptionHelperImpl

--- app/src/test/java/ayds/songinfo/moredetails/fulllogic/presentation/DetailsDescriptionHelperImplTest.kt
+++ app/src/test/java/ayds/songinfo/moredetails/fulllogic/presentation/DetailsDescriptionHelperImplTest.kt
@@ -1,24 +1,117 @@
 package ayds.songinfo.moredetails.fulllogic.presentation
 
 import ayds.songinfo.moredetails.fulllogic.domain.entity.ArtistDetails
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 class DetailsDescriptionHelperImplTest {
 
-    private val detailsDescriptionHelper = DetailsDescriptionHelperImpl()
+    private val detailsDescriptionHelper: DetailsDescriptionHelper = DetailsDescriptionHelperImpl()
 
     @Test
     fun `get description with empty artist details`() {
         val emptyArtistDetails = ArtistDetails(
             "",
             "",
-            "",
-            isLocallyStored = false
+            ""
         )
 
         val result = detailsDescriptionHelper.getDescription(emptyArtistDetails)
 
-        assertEquals("<html><div width=400><font face=\"arial\"><b></b></font></div></html>", result)
+        val expected = "<html><div width=400><font face=\"arial\">" +
+                            "<b></b>" +
+                            "</font></div></html>"
+
+        assertEquals(expected, result)
+    }
+
+     @Test
+     fun `given a local artist it should return the description`() {
+         val artistDetails = ArtistDetails(
+             "Green Day",
+             "biography",
+             "url",
+             isLocallyStored = true
+         )
+
+         val result = detailsDescriptionHelper.getDescription(artistDetails)
+
+         val expected = "<html><div width=400><font face=\"arial\">" +
+                            "[*]biography" +
+                            "</font></div></html>"
+
+         assertEquals(expected, result)
+     }
+
+    @Test
+    fun `given a non local artist it should return the description`() {
+        val artistDetails = ArtistDetails(
+            "Green Day",
+            "biography",
+            "url",
+            isLocallyStored = false
+        )
+
+        val result = detailsDescriptionHelper.getDescription(artistDetails)
+
+        val expected = "<html><div width=400><font face=\"arial\">" +
+                "biography" +
+                "</font></div></html>"
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `biography should highlight artist name regardless of case`() {
+        val artistDetails = ArtistDetails(
+            "Green Day",
+            "Green Day is a band. green day is popular. GREEN DAY rocks.",
+            "url"
+        )
+
+        val result = detailsDescriptionHelper.getDescription(artistDetails)
+
+        val expected = "<html><div width=400><font face=\"arial\">" +
+                "<b>GREEN DAY</b> is a band. <b>GREEN DAY</b> is popular. <b>GREEN DAY</b> rocks." +
+                "</font></div></html>"
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `biography should convert new lines to HTML breaks`() {
+        val artistDetails = ArtistDetails(
+            "Beatles",
+            "The Beatles are a legendary band.\nThey changed the music world.",
+            "url"
+        )
+
+        val result = detailsDescriptionHelper.getDescription(artistDetails)
+
+        val expected = "<html><div width=400><font face=\"arial\">" +
+                "The <b>BEATLES</b> are a legendary band.<br>They changed the music world." +
+                "</font></div></html>"
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `description should be generated correctly using mocked artist details`() {
+        val artistDetails: ArtistDetails = mockk()
+
+        every { artistDetails.artistName } returns "Mocked Artist"
+        every { artistDetails.biography } returns "This is a biography of Mocked Artist."
+        every { artistDetails.articleUrl } returns "http://mocked.url"
+        every { artistDetails.isLocallyStored } returns false
+
+        val result = detailsDescriptionHelper.getDescription(artistDetails)
+
+        val expected = "<html><div width=400><font face=\"arial\">" +
+                "This is a biography of <b>MOCKED ARTIST</b>." +
+                "</font></div></html>"
+
+        assertEquals(expected, result)
     }
 
 }

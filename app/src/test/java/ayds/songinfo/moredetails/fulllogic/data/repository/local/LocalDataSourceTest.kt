e2e4493991f11c291493120458b7c30d0d7d6ba1
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-05-19 18:28:12 -0300

    se agrego otro test a LocalDataSourceTest

--- app/src/test/java/ayds/songinfo/moredetails/fulllogic/data/repository/local/LocalDataSourceTest.kt
+++ app/src/test/java/ayds/songinfo/moredetails/fulllogic/data/repository/local/LocalDataSourceTest.kt
@@ -1,12 +1,12 @@
 package ayds.songinfo.moredetails.fulllogic.data.repository.local
 
 import ayds.songinfo.moredetails.fulllogic.domain.entity.ArtistDetails
+import io.mockk.Runs
 import io.mockk.every
+import io.mockk.just
 import io.mockk.mockk
-import io.mockk.verify
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertNotNull
-import junit.framework.TestCase.assertNull
 import org.junit.Test
 
 class LocalDataSourceTest{
@@ -42,5 +42,30 @@ class LocalDataSourceTest{
 
         assertNotNull(articleEntityTest)
         assertEquals(result,artistDetailsTest)
+    }
+
+    @Test
+    fun `when  a method isertArtist, insert a valida articleEntity`(){
+        val articleEntityTest = ArticleEntity(
+            "pedroMockk",
+            "this's a biography of pedroMockk",
+            "url of pedroMockk"
+        )
+        every { articleDatabase.ArticleDao().insertArticle(articleEntityTest) } just Runs
+        every { articleDatabase.ArticleDao().getArticleByArtistName("pedroMockk") } returns articleEntityTest
+
+        localDataSource.insertArtist(ArtistDetails(
+            artistName = "pedroMockk",
+            biography = "this's a biography of pedroMockk",
+            articleUrl = "url of pedroMockk"
+        ))
+
+        val result = localDataSource.getArticleByArtistName("pedroMockk")
+
+        assertNotNull(result)
+        assertEquals(articleEntityTest.artistName, result?.artistName)
+        assertEquals(articleEntityTest.biography, result?.biography)
+        assertEquals(articleEntityTest.articleUrl, result?.articleUrl)
+
     }
 }

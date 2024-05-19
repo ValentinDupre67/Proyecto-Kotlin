01efde2c0f91918f1813ea04b23f0579bee12af0
Valentin Dupre <84106558+ValentinDupre67@users.noreply.github.com>
2024-05-19 17:24:58 -0300

    se agrego un test a LocalDataSourceTest

--- app/src/test/java/ayds/songinfo/moredetails/fulllogic/data/repository/local/LocalDataSourceTest.kt
+++ app/src/test/java/ayds/songinfo/moredetails/fulllogic/data/repository/local/LocalDataSourceTest.kt
@@ -1,9 +1,12 @@
 package ayds.songinfo.moredetails.fulllogic.data.repository.local
 
+import ayds.songinfo.moredetails.fulllogic.domain.entity.ArtistDetails
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import junit.framework.TestCase.assertNull
 import org.junit.Test
 
 class LocalDataSourceTest{
@@ -18,5 +21,26 @@ class LocalDataSourceTest{
         val result = localDataSource.getArticleByArtistName(artistName)
 
         assertEquals(result,null)
+    }
+
+    @Test
+    fun `when the articleEntity in not null, getArticleByArtistName should return a valid ArtistDetails`(){
+        val articleEntityTest = ArticleEntity(
+            "nameMockk",
+            "this's a biography",
+            "url of nameMockk"
+        )
+        val artistDetailsTest = ArtistDetails(
+            "nameMockk",
+            "this's a biography",
+            "url of nameMockk",
+            isLocallyStored = false
+        )
+        every { articleDatabase.ArticleDao().getArticleByArtistName(artistName) }returns articleEntityTest;
+
+        val result = localDataSource.getArticleByArtistName(artistName)
+
+        assertNotNull(articleEntityTest)
+        assertEquals(result,artistDetailsTest)
     }
 }
